cf8b82f43388d9439b8fbe406688ec4b6b793aa1
José Maria Alcántara <jm.alcantara@cesariglesias.com.do>
2020-05-03 17:27:39 -0400

    After Basic Binding

--- HelloWorld/HelloWorld/GreetPage.xaml.cs
+++ HelloWorld/HelloWorld/GreetPage.xaml.cs
@@ -19,9 +19,6 @@ namespace HelloWorld
             slider.Value = 0.5;
         }
 
-        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
-        {
-            label.Text = String.Format("Value is {0:F2}", e.NewValue);
-        }
+        
     }
 }
