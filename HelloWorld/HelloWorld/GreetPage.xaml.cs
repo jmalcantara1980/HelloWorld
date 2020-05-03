7cd50d445e9ae424f1e7f62e7d539784638336f8
José Maria Alcántara <jm.alcantara@cesariglesias.com.do>
2020-05-03 17:12:59 -0400

    Add project files.

---
+++ HelloWorld/HelloWorld/GreetPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GreetPage : ContentPage
+    {
+        public GreetPage()
+        {
+            InitializeComponent();
+
+            slider.Value = 0.5;
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            label.Text = String.Format("Value is {0:F2}", e.NewValue);
+        }
+    }
+}
